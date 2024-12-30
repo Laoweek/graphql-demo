7b0386d834083d88ba80ad576203aa59d27c6f7b
Simon Li <laoweek@piran.xyz>
2024-12-31 01:18:16 +0800

    add submodule

--- .github/actions/commit-files.mjs
+++ .github/actions/commit-files.mjs
@@ -5,40 +5,40 @@ const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 const branch = process.env.GITHUB_REF.replace('refs/heads/', '');
 
 const { data } = await octokit.rest.repos.getBranch({
-    owner,
-    repo,
-    branch
+  owner,
+  repo,
+  branch
 });
 
 const res = await octokit.graphql(
-    `
+  `
     mutation($input: CreateCommitOnBranchInput!){
       createCommitOnBranch(input: $input) {
         commit { url }
       }
     }
     `,
-    {
-        input: {
-            branch: {
-                repositoryNameWithOwner: `${owner}/${repo}`,
-                branchName: branch
-            },
-            fileChanges: {
-                additions: [
-
-                ],
-                deletions: [{
-                    "path": "submodule-demo",
-                }]
-            },
-            message: {
-                headline: `test`,
-                body: `test body`
-            },
-            expectedHeadOid: data.commit.sha
-        }
+  {
+    input: {
+      branch: {
+        repositoryNameWithOwner: `${owner}/${repo}`,
+        branchName: branch
+      },
+      fileChanges: {
+        additions: [
+            {
+                "path": "submodule-demo",
+                "contents": Buffer.from("9dc0d11e6f5ea9fd25df9c5cda997e10728cc1cd").toString('base64')
+              }
+        ]
+      },
+      message: {
+        headline: `test`,
+        body: `test body`
+      },
+      expectedHeadOid: data.commit.sha
     }
+  }
 )
 
 console.log(JSON.stringify(res, null, 2))
